7c4bf9d3b2766cb1d85436c970a99faf8eacc039
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-04-16 12:45:24 +0800

    Created the components needed in our app project including the home and navigation

---
+++ src/app/comps/water-conservation-guide/water-conservation-guide.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-water-conservation-guide',
+  imports: [],
+  templateUrl: './water-conservation-guide.component.html',
+  styleUrl: './water-conservation-guide.component.css'
+})
+export class WaterConservationGuideComponent {
+
+}
