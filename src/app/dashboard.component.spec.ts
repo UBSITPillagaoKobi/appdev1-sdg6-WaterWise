9b78ab14b25357786d362ad0a2ac4448ef8b79a3
Josh Codod <20243506@s.ubaguio.edu>
2026-04-16 12:29:42 +0800

    test: add DashboardComponent spec (commit 5/5)

---
+++ src/app/dashboard.component.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({ imports: [DashboardComponent] }).compileComponents();
+  });
+
+  it('should create the dashboard component', () => {
+    const fixture = TestBed.createComponent(DashboardComponent);
+    const comp = fixture.componentInstance;
+    expect(comp).toBeTruthy();
+  });
+});
