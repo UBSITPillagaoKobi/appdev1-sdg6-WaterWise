b72a3d4b9c6d0da9ed8e84c0295010ba5f425b4d
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-04-20 08:15:55 +0800

    Removed the default template for the app html file and added the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
