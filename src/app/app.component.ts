a5148d978e5a036fb296df72b178d5f0337a2d6e
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-05-04 08:44:38 +0800

    Fixed merged conflicts and code errors to run the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { HeaderComponent } from './header.component';
 import { DashboardComponent } from './dashboard.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, DashboardComponent],
-=======
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
->>>>>>> 89d88bddd60c8f96c6c7569edb76d849073e27b2
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
