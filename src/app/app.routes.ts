551b748a6334f8e1b6eaf2266267df68e5adbc32
UBSITPillagaoKobi <20187699@s.ubaguio.edu>
2026-05-07 11:34:11 +0800

    fixed some errors for it to run

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { UsageComponent } from './usage.component';
 import { AuthGuard } from './guards/auth.guard';
-import { WaterQualityComponent } from './comps/water-quality-monitor/water-quality-monitor.component';
+import { WaterQualityMonitorComponent } from './comps/water-quality-monitor/water-quality-monitor.component';
 import { WaterConservationGuideComponent } from './comps/water-conservation-guide/water-conservation-guide.component';
 import { SanitationAccessTrackerComponent } from './comps/sanitation-access-tracker/sanitation-access-tracker.component';
 import { ProfileComponent } from './comps/profile/profile.component';
@@ -11,7 +11,7 @@ export const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'usage', component: UsageComponent, canActivate: [AuthGuard] },
-	{ path: 'water-quality', component: WaterQualityComponent, canActivate: [AuthGuard] },
+	{ path: 'water-quality', component: WaterQualityMonitorComponent, canActivate: [AuthGuard] },
 	{ path: 'water-conservation', component: WaterConservationGuideComponent, canActivate: [AuthGuard] },
 	{ path: 'sanitation', component: SanitationAccessTrackerComponent, canActivate: [AuthGuard] },
 	{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
