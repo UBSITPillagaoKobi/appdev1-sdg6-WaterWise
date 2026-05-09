1b7e1e2f4bec9a8b2589bcb411573348fb9bd68c
AlcedoEllyn <alcedoellyn22@gmail.com>
2026-05-09 21:33:44 +0700

    Created a new component

---
+++ src/app/comps/facility-detail/facility-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facility-detail',
+  imports: [],
+  templateUrl: './facility-detail.html',
+  styleUrl: './facility-detail.css',
+})
+export class FacilityDetail {
+
+}
